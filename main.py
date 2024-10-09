d989684e2b4314233b75724f6a728146c5666007
skullboyG <skullboy.dev@gmail.com>
2024-10-09 09:34:11 -0500

    se actualiza la funcion create task con persitencia de datos utilizando un archivo externo

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 #!/user/bin/python 
-import pickle
-
-list_task=[]
 
 def create_task(task=str):
     if len(task) == 0:
-        return "empty task"
+        return "Empty task"
     else:
-        list_task.append(task)
-        return "ok"
+        with open("task", "a") as file:
+            file.write(f"{task} \n")
+    return "Save task"
 
 
 task = input("> ")
-
-print(create_task(task=task))
+create_task(task=task)
