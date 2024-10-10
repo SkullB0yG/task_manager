551156fb49be37f5f05c036846b1c306c48e5185
skullboyG <skullboy.dev@gmail.com>
2024-10-09 20:35:25 -0500

    se agrega la funcion view_task

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ def create_task(task=str):
             file.write(f"{task} \n")
     return "Save task"
 
-task = input("> ")
-create_task(task=task)
+def view_task():
+    with open("pending_tasks", "r") as file:
+        count = 1
+        for linea in file:
+            print(f"[{count}] {linea.strip()}")
+            count = count + 1 
+
+#task = input("> ")
+#create_task(task=task)
+
+view_task()
