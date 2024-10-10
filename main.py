45335698c6b35ab17991f9eac5ca9f8cb1a87045
skullboyG <skullboy.dev@gmail.com>
2024-10-09 23:24:28 -0500

    se agrega la funcion comlete_task

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ def view_task():
     try:
         with open(filename, "r") as file:
             count = 0
+            print("[ID  TAKS]")
             for linea in file:
-                print(f"[{count}] {linea.strip()}")
+                print(f" {count}   {linea.strip()}")
                 count = count + 1 
+            file.close()
     except FileExistsError:
         print("Empty list: no tasks have been created!")
 
@@ -27,7 +29,7 @@ def delete_task(id = int):
             contenido = file.readlines()
             if 0<=id < len(contenido):
                 del contenido[id]
-                print("Task completed")
+                print("Task delete")
             else:
                 print("Id Error: id not identified (try running view command to see pending tasks)")
 
@@ -38,7 +40,37 @@ def delete_task(id = int):
         print("Empty list: no tasks have been created!")
 
 
-task = int(input("> "))
-complite_task(id=task)
+def complete_task(task_complete = str):
+    try:
+        with open(filename, "r") as file:
+            contenido = file.readlines()
+            loops = 0
+            for id, element in enumerate(contenido):
+
+                if task_complete in element and  not "COMPLETE" in element:
+                    contenido[id] = element.strip() + " " + " COMPLETE" + "\n"
+        
+                    with open(filename, "w") as file:
+                        file.writelines(contenido)
+                        file.close() 
+                    print("Task marked as complete") 
+                    break
+
+                if task_complete in element and "COMPLETE" in element:
+                    print("The task is now complete")
+                    break
+                
+                loops += 1
+
+                if loops == len(contenido):
+                    print("Task not Found Error: (try running view command to see pending tasks)")
+    except FileExistsError:
+        print("Empty list: no tasks have been created!")
+
+
+
+#task = input("> ")
 #create_task(task=task)
-#view_task()
+#complete_task(task_complete=task)    
+#delete_task(id=task)
+view_task()
