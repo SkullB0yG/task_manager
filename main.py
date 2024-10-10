f197bbbee1b049220d16a65eda98241085a55c69
skullboyG <skullboy.dev@gmail.com>
2024-10-09 22:03:31 -0500

    se agrega la funcion delete_task y se depuran algunos error de las funciones anteriores incluyendo la funcion delete_task

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
 #!/user/bin/python 
 
+
+filename = "pending_tasks"
+
 def create_task(task=str):
     if len(task) == 0:
-        return "Empty task"
+        print ("Empty task")
     else:
-        with open("pending_tasks", "a") as file:
+        with open(filename,"a") as file:
             file.write(f"{task} \n")
-    return "Save task"
+    print("Save task")
 
 def view_task():
-    with open("pending_tasks", "r") as file:
-        count = 1
-        for linea in file:
-            print(f"[{count}] {linea.strip()}")
-            count = count + 1 
+    try:
+        with open(filename, "r") as file:
+            count = 0
+            for linea in file:
+                print(f"[{count}] {linea.strip()}")
+                count = count + 1 
+    except FileExistsError:
+        print("Empty list: no tasks have been created!")
 
-#task = input("> ")
+def delete_task(id = int):
+    try:
+        with open(filename, "r") as file:
+            contenido = file.readlines()
+            if 0<=id < len(contenido):
+                del contenido[id]
+                print("Task completed")
+            else:
+                print("Id Error: id not identified (try running view command to see pending tasks)")
+
+        with open(filename, "w") as file:
+            file.writelines(contenido)
+            file.close()
+    except FileExistsError:
+        print("Empty list: no tasks have been created!")
+
+
+task = int(input("> "))
+complite_task(id=task)
 #create_task(task=task)
-
-view_task()
+#view_task()
